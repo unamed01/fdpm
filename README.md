23f224d584fdd722667c4cfb642291108b5f774e
unamed <aiden1999999@proton.me>
2026-06-03 19:05:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# dpm (deterministic password manager)
+
+minimal alpha of fully deterministic password manager it fixes two main problems of traditional password managers.
+
+1. someone steals your password vault and tries to brute force it offline (even though this is insanely difficult to pull off if you have a strong master password)
+2. backing up password vault/accessing passwords on any machine without having a intricate backup system
+it fixes them by fully generating your passwords on the fly derived from the master-password and the service name directly same input will always generate the same output no matter what machine you're on, without any external servers or services needed. also means no metadata is ever stored even on disk about what services you have an account on its the **stupidiest password manager on earth entirely by design**. also fixes other problems in other fully deterministic password managers.
+3. most other deterministic password mangers usually derive the password directly from the master itself meaning that each password that gets compromised gives more info to an attacker about your master password
+4. metadata problem, other password managers still store some metadata about the services you have accounts in for convinience while dpm does not by design.
+it's just a rust binary everything gets generated entirely on the fly nothing gets stored on disk at all it needs literally nothing to work other than the binary itself and a cpu
+
+## alpha build
+
+this is mostly a proof of concept right now this shouldn't be used in production while the concepts are sound it still has rough edges that will get fixed later this is only a testing build for now
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use argon2::{Argon2, PasswordHasher, password_hash::SaltString};
+use rand::rngs::StdRng;
+use rand::{RngExt, SeedableRng};
+use std::{env, io};
+use zeroize::Zeroize;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    println!("input pass");
+    let mut pass = String::new();
+    io::stdin().read_line(&mut pass).unwrap();
+    // let pass = pass.trim();
+    let salt = SaltString::from_b64("bXlzaXRlLmNvbQ").unwrap();
+    let argon2 = Argon2::default();
+    let argon = argon2.hash_password(pass.as_bytes(), &salt).unwrap();
+    pass.zeroize();
+    let seed: [u8; 32] = argon.hash.unwrap().as_bytes().try_into().unwrap();
+    let mut rng = <StdRng as SeedableRng>::from_seed(seed);
+    let verify_string = get_pass_from_rng(&mut rng, Some(4));
+    println!(
+        "your verify string is:{verify_string} \nif it looks correct press enter if not input anything else."
+    );
+    let mut ok = String::new();
+    io::stdin().read_line(&mut ok).unwrap();
+    if ok != "\n" {
+        return Ok(());
+    }
+    let service = if args.get(1).is_none() {
+        let mut service = String::new();
+        println!("input desired service");
+        io::stdin().read_line(&mut service).unwrap();
+        service
+    } else {
+        args[1].clone()
+    };
+    let mut password = match_2_service(service.as_str(), &seed);
+    println!("your pasword is {}", password);
+    password.zeroize();
+    Ok(())
+}
+fn get_pass_from_rng(rng: &mut StdRng, length: Option<i16>) -> String {
+    let length = length.unwrap_or(128);
+    let mut output = Vec::with_capacity(128);
+    for _ in 0..length {
+        let a = char::from_u32(rng.random_range(32..127)).unwrap();
+        output.push(a);
+    }
+    output.iter().collect::<String>()
+}
+fn match_2_service(service: &str, pass_seed: &[u8; 32]) -> String {
+    let argon2 = Argon2::default();
+    let salt = SaltString::from_b64("bXlzaXRlLmNvbQ").unwrap();
+    let mut password_and_service = Vec::new();
+    password_and_service.extend_from_slice(pass_seed);
+    password_and_service.extend_from_slice(service.as_bytes());
+    let hash = argon2
+        .hash_password(password_and_service.as_slice(), &salt)
+        .unwrap()
+        .hash
+        .unwrap();
+    let seed = hash.as_bytes().try_into().unwrap();
+    let mut rng = <StdRng as SeedableRng>::from_seed(seed);
+    get_pass_from_rng(&mut rng, None)
+}
