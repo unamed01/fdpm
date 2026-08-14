fdbe2acf1ab53a092bc712c0c0ef3dc7f75dedc4
unamed01 <unamed01@proton.me>
2026-08-13 21:06:15 -0400

    clipboard integration with auto clearing + performance improvements

--- README.md
+++ README.md
@@ -1,33 +1,39 @@
 # dpm (deterministic password manager)
 
-minimal fully deterministic password manager in rust, this approach fixes two main problems of traditional password managers.
+Minimal fully deterministic password manager in rust, it's approach fixes two main problems of traditional password managers.
 
-1. someone steals your password vault and tries to brute force it offline (even though this is insanely difficult to pull off if you have a strong master password).
+1. someone steals your password vault and tries to brute force it offline (even though it's mostly solved when using strong passphrase).
 2. backing accessing passwords on any machine without a intricate backup system/accessing passwords in a airgapped machine.
 
-dpm fixes them by fully generating your passwords on the fly derived from the master-password plus service name, the same input will always generate the same output no matter what machine you're on, without any external servers or services needed.
+dpm fixes them by fully generating your passwords on the fly derived from the master-password plus service name, the same input will always generate the same output no matter what machine you're on, without any external servers or services needed. This is why you'll never need a backup since there is NEVER anything to backup.
 
-also means no metadata is ever stored even on disk about what services you have an account on. dpm is the **stupidiest password manager on earth by design**. also fixes other problems in other fully deterministic password managers.
+Also means no metadata is ever stored even on disk about what services you have an account on. dpm is the **stupidiest password manager on earth by design**. Also fixes other problems in other fully deterministic password managers.
 
 1. most other deterministic password mangers usually derive the password directly from the master itself meaning that each password that inevitably gets compromised gives more info about your master password
 2. metadata problem, other password managers still store some metadata about the services you have accounts in for convinience while dpm does not by design.
 
-dpm is literally just a rust binary everything gets generated entirely on the fly nothing gets stored on disk at all. It needs nothing to work other than the binary itself, a cpu and ram (optional) also fixes information leakage problem by before deriving the passwords from master password hash + service name it hashes both together with Argon2 then derives the individual password from that. Because of the way dpm is designed an attacker that compromises your system and finds a dpm binary, could not start to brute force your passwords without having compromised at least one password first which raises the bar a lot, and to start to brute force
+dpm is just a rust binary everything gets generated entirely on the fly nothing ever gets stored on disk at all. It needs nothing to work other than the binary itself a cpu and ram no backup is ever necessary other than binary itself only if you change the SALT const.
+
+Fixes information leakage problem by before deriving the passwords from master password hash + service name it hashes both together with Argon2 then derives the individual password from that. Because of the way dpm is designed an attacker that compromises your system and finds a dpm binary, could not start to brute force your passwords without having compromised at least one password first which raises the bar A LOT. Even if your verification string (4 chars that get printed to the screen to ensure you've typed in your master password correctly) is compromised attacker can't reliably brute force based on that since it's designed to be low entropy and collision prone so an attacker can't dream to use it to brute force your passwords.
 
 ## usage
 
 ### recommended
 
-its recommended you change the salt const
+It's recommended you change the salt const for personal use
 
 ```
 const SALT: &[u8] = b"deci5Dzx+PvvvIaS7osBVgUVByBECbOfq5zZRJD8aD8="
 ```
 
-change to whatever use head -c 32 /dev/urandom | base64 and paste what you get into the ""
+Change to some random string use head -c 32 /dev/urandom | base64 and paste what you get into the "" as a sane default
 
-test it out by running its an extremely simple app by design
+Test it out by running it. It's an extremely simple app by design 
 
 ```
 cargo run
+```
+or run it with an argv to set the service explicitly (be careful with leaking service metadata to shell history prefix the command with a space)
+```bash
+ cargo run -- github.com # or whatever service.
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use arboard::Clipboard;
 use argon2::Argon2;
 use rand::rngs::StdRng;
 use rand::{RngExt, SeedableRng};
-use rpassword::read_password;
-use std::io::Write;
+use std::io::{Write, stdout};
+use std::thread::sleep;
+use std::time::Duration;
 use std::{env, io};
 use zeroize::Zeroize;
 
@@ -13,26 +15,32 @@ const SALT: &[u8] = b"deci5Dzx+PvvvIaS7osBVgUVByBECbOfq5zZRJD8aD8=";
 #[allow(clippy::main_recursion)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 && matches!(args[1].as_str(), "-h" | "--help") {
+        print_help(&args[0]);
+        return Ok(());
+    }
     println!("input pass (won't be echoed)");
-    print!(">");
-    io::stdout().flush().unwrap();
-    let mut pass = read_password()?;
+    let mut pass = rpassword::prompt_password(">")?;
     let argon2 = Argon2::default();
     let mut seed = [0u8; 32];
     argon2
         .hash_password_into(pass.as_bytes(), SALT, &mut seed)
-        .unwrap();
+        .expect("couldn't hash password.");
     pass.zeroize();
     let mut rng = <StdRng as SeedableRng>::from_seed(seed);
+    seed.zeroize();
+
+    //verify string is how I choose to solve the problem of inputting wrong password and not ever knowing its only 4 letters so pretty easy to remember
+    //this allows for verifying your password is correct while
     let verify_string = get_pass_from_rng(&mut rng, Some(4));
-    println!("your verify string is:{verify_string}");
+    println!("your verify string is:{}", verify_string);
     println!("if this looks correct press enter if not input anything else.");
     let mut ok = String::new();
     io::stdin().read_line(&mut ok).unwrap();
     if ok != "\n" {
         let _ = main();
         return Ok(());
-    };
+    }
     let service = if args.get(1).is_none() {
         let mut service = String::new();
         println!("input desired service");
@@ -41,18 +49,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     } else {
         args[1].clone()
     };
-    let mut password = match_2_service(service.as_str(), &seed);
-    seed.zeroize();
-    println!("your pasword is {}", password);
+    let mut clipboard = match Clipboard::new() {
+        Ok(k) => k,
+        Err(e) => {
+            seed.zeroize();
+            eprintln!("{e}");
+            panic!("couldn't get clipboard handle")
+        }
+    };
+    let mut pass_seed = [0u8; 32];
+    rng.fill(&mut pass_seed);
+    let mut password = match_2_service(service.as_str(), &pass_seed);
+    pass_seed.zeroize();
+    clipboard
+        .set_text(&password)
+        .unwrap_or_else(|_| password.zeroize());
+    println!("password copied to clipboard");
     password.zeroize();
+    for secs in (0..=6).rev() {
+        print!("\rclearing clipboard in: {}", secs);
+        sleep(Duration::new(1, 0));
+        stdout().flush()?;
+    }
+    clipboard.clear()?;
+    println!();
+    println!("clipboard cleared.");
     Ok(())
 }
 fn get_pass_from_rng(rng: &mut StdRng, length: Option<i16>) -> String {
-    let length = length.unwrap_or(64);
+    let length = match length {
+        Some(s) => s,
+        // fixes fingerprinting, each password is a different length so server can't distinguish us
+        // from anyone else using a traditional password manager.
+        None => rng.random_range(44..126),
+    };
     let mut string = String::new();
     for _ in 0..length {
         let a = char::from_u32(rng.random_range(32..127)).unwrap();
-        string = string + &a.to_string();
+        string.push(a);
     }
     string
 }
@@ -68,4 +102,31 @@ fn match_2_service(service: &str, pass_seed: &[u8; 32]) -> String {
     let mut rng = <StdRng as SeedableRng>::from_seed(seed);
     seed.zeroize();
     get_pass_from_rng(&mut rng, None)
+}
+fn print_help(program_name: &String) {
+    println!("deterministic password manager (DPM):");
+    println!("  takes a password and runs it trough a cryptographically secure RNG.");
+    println!(
+        "  fully deterministically generating passwords, that never touch non volatile in ANY form."
+    );
+    println!(
+        "  mixes both master password AND service your service name to generate each individual password"
+    );
+    println!(
+        "  stores ZERO metadata at all, so you must provide both master password and service name each time"
+    );
+    println!(
+        "  this password manger is fully deniable. this password manager leaves no traces at all."
+    );
+    println!(
+        "  after this programs closes, nothing on the system (other the binary itself) points to you ever had of your passwords on this system"
+    );
+    println!(
+        "  allowing for use in any computer on earth no vault sharing required (even fully airgaped ones) just needs the binary.."
+    );
+    println!("USAGE:");
+    println!("  {program_name} -h | --help    prints this message");
+    println!(
+        "  {program_name} <service>      pass on service name as a shell argument (possibly leaks service name to shell history make sure to prefix command by space to prevent it). ",
+    );
 }
